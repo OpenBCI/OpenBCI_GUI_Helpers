c781ea59e4e167bf02ba3c2e7c56d5434cb77dbe
Andrey Parfenov <a1994ndrey@gmail.com>
2020-02-07 17:20:18 +0300

    wip try to add autosidcovery for wifi shield
    Signed-off-by: Andrey Parfenov <a1994ndrey@gmail.com>

---
+++ java-package/openbci_gui_helpers/src/main/java/com/openbci/examples/TestWifiDiscovery.java
@@ -0,0 +1,16 @@
+package com.openbci.examples;
+
+import java.io.IOException;
+import java.util.List;
+
+import com.vmichalak.protocol.ssdp.Device;
+import com.vmichalak.protocol.ssdp.SSDPClient;
+
+public class TestWifiDiscovery
+{
+    public static void main (String[] args) throws IOException
+    {
+        List<Device> devices = SSDPClient.discover (3000, "urn:schemas-upnp-org:device:Basic:1");
+        System.out.println (devices.size () + " devices found");
+    }
+}
