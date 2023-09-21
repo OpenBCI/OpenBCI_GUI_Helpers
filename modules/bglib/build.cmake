b2a88392c0c79c144511377a991be32fff79a2af
Philip Pitts <84428015+philippitts@users.noreply.github.com>
2023-09-21 17:02:25 -0400

    Modularize cmake configurations

---
+++ modules/bglib/build.cmake
@@ -0,0 +1,30 @@
+SET(GANGLION_LIB "GanglionScan")
+
+add_library(
+    ${GANGLION_LIB} SHARED
+    modules/bglib/src/callbacks.cpp
+    modules/bglib/src/cmd_def.cpp
+    modules/bglib/src/stubs.cpp
+    modules/bglib/src/uart.cpp
+    modules/bglib/src/openbci_gui_helpers.cpp
+)
+
+target_include_directories(
+    ${GANGLION_LIB} PUBLIC
+    3rdparty/json
+    modules/bglib/include
+)
+
+set_property(TARGET ${GANGLION_LIB} PROPERTY POSITION_INDEPENDENT_CODE ON)
+
+set_target_properties(${GANGLION_LIB}
+    PROPERTIES
+
+    ARCHIVE_OUTPUT_DIRECTORY_RELEASE ${CMAKE_HOME_DIRECTORY}/java-package/openbci_gui_helpers/src/main/resources/
+    LIBRARY_OUTPUT_DIRECTORY_RELEASE ${CMAKE_HOME_DIRECTORY}/java-package/openbci_gui_helpers/src/main/resources/
+    RUNTIME_OUTPUT_DIRECTORY_RELEASE ${CMAKE_HOME_DIRECTORY}/java-package/openbci_gui_helpers/src/main/resources/
+
+    ARCHIVE_OUTPUT_DIRECTORY_DEBUG ${CMAKE_HOME_DIRECTORY}/java-package/openbci_gui_helpers/src/main/resources/
+    LIBRARY_OUTPUT_DIRECTORY_DEBUG ${CMAKE_HOME_DIRECTORY}/java-package/openbci_gui_helpers/src/main/resources/
+    RUNTIME_OUTPUT_DIRECTORY_DEBUG ${CMAKE_HOME_DIRECTORY}/java-package/openbci_gui_helpers/src/main/resources/
+)
---
+++ modules/native-ble/build.cmake
@@ -0,0 +1,32 @@
+SET(OPENBCI_GANGLION_NATIVE_LIB "GanglionNativeScan")
+
+add_library(
+    ${OPENBCI_GANGLION_NATIVE_LIB} SHARED
+    modules/native-ble/src/openbci_gui_native_helpers.cpp
+)
+
+target_include_directories(
+    ${OPENBCI_GANGLION_NATIVE_LIB} PUBLIC
+    modules/native-ble/include
+    3rdparty/json
+    3rdparty/SimpleBLE/simpleble/include/simpleble
+)
+
+target_link_libraries(
+    ${OPENBCI_GANGLION_NATIVE_LIB} PRIVATE
+    simpleble
+)
+
+set_property(TARGET ${OPENBCI_GANGLION_NATIVE_LIB} PROPERTY POSITION_INDEPENDENT_CODE ON)
+
+set_target_properties(${OPENBCI_GANGLION_NATIVE_LIB}
+    PROPERTIES
+
+    ARCHIVE_OUTPUT_DIRECTORY_RELEASE ${CMAKE_HOME_DIRECTORY}/java-package/openbci_gui_helpers/src/main/resources/
+    LIBRARY_OUTPUT_DIRECTORY_RELEASE ${CMAKE_HOME_DIRECTORY}/java-package/openbci_gui_helpers/src/main/resources/
+    RUNTIME_OUTPUT_DIRECTORY_RELEASE ${CMAKE_HOME_DIRECTORY}/java-package/openbci_gui_helpers/src/main/resources/
+
+    ARCHIVE_OUTPUT_DIRECTORY_DEBUG ${CMAKE_HOME_DIRECTORY}/java-package/openbci_gui_helpers/src/main/resources/
+    LIBRARY_OUTPUT_DIRECTORY_DEBUG ${CMAKE_HOME_DIRECTORY}/java-package/openbci_gui_helpers/src/main/resources/
+    RUNTIME_OUTPUT_DIRECTORY_DEBUG ${CMAKE_HOME_DIRECTORY}/java-package/openbci_gui_helpers/src/main/resources/
+)
